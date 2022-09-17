5412930b3b453bcd53556650ac523cb5c9d4abf2
Abdulbasit sadiq <76534194+abdbee@users.noreply.github.com>
2022-09-17 20:30:27 +0100

    joining mutable and immutable references together in one scope

--- ownership_4/src/main.rs
+++ ownership_4/src/main.rs
@@ -2,5 +2,6 @@ mod mutable_reference;
 
 fn main() {
     mutable_reference::mutable();
+    mutable_reference::mutable_and_immutable();
 
 }
--- ownership_4/src/mutable_reference.rs
+++ ownership_4/src/mutable_reference.rs
@@ -17,3 +17,16 @@ pub fn change(y: &mut String) {
 }
 
 // but you can always use curly braces to create a new scope for another mutable reference of the same data. 
+
+pub fn mutable_and_immutable() {
+    let mut s = String::from("hello");
+    
+    //creating multiple immutable references is not a problem. the data isn't being changed anyway.
+    let r1 = &s;
+    let r2 = &s; 
+    //however, we cannot have an immutable reference together with a mutable reference in the same scope.
+    // this is because those using the immutable reference might not be expecting that the value will change.
+    //let r3 = &mut s; //uncommenting this and printing r1, r2 and r3 will print an error.
+
+    // println!("{}, {}, {}", r1, r2, r3);
+}
