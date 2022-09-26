dee1a30648629e96cf1503301adc32ad4924d0b0
Abdulbasit sadiq <76534194+abdbee@users.noreply.github.com>
2022-09-26 07:13:19 +0100

    running single test, filtering to run multiple tests, ignoring tests

--- Writing_automated_tests_11/src/test_subset.rs
+++ Writing_automated_tests_11/src/test_subset.rs
@@ -1,4 +1,26 @@
+pub fn add_two(x: u32, y:u32)-> u32 {
+    x + y
+ }
+
 #[cfg(test)]
 pub mod tests {
-    
+    use super::*;
+
+    #[test]
+    fn is_it_equal1() {
+        assert_eq!(4, add_two(2,2));
+    }
+
+    #[test]
+    #[ignore] //this ignores this test when running tests
+    fn is_it_equal2() {
+        let value = 4;
+        assert_eq!(value, add_two(2,3), "{} was printed instead of {}", value, add_two(2,3));
+    }
 }
+
+//to only run a particular test, do: cargo test <test_function_name>
+// you can specify part of the test name, and any tests that have that part gets run eg, cargo test is
+
+// to ignore a test while running tests, add the "ignore attribute" before writing the test
+// to test only ignored tests, run: cargo test -- --ignored
--- Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb.d
+++ Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb.d
@@ -1,9 +1,10 @@
-/Users/abdulbee/Desktop/Samples/TRPL_Book/Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb: src/lib.rs src/assert_for_result.rs src/testing_equality.rs src/custom_messages.rs src/result_in_tests.rs
+/Users/abdulbee/Desktop/Samples/TRPL_Book/Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb: src/lib.rs src/assert_for_result.rs src/testing_equality.rs src/custom_messages.rs src/result_in_tests.rs src/test_subset.rs
 
-/Users/abdulbee/Desktop/Samples/TRPL_Book/Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb.d: src/lib.rs src/assert_for_result.rs src/testing_equality.rs src/custom_messages.rs src/result_in_tests.rs
+/Users/abdulbee/Desktop/Samples/TRPL_Book/Writing_automated_tests_11/target/debug/deps/Writing_automated_tests_11-cc1ec0dc7634cacb.d: src/lib.rs src/assert_for_result.rs src/testing_equality.rs src/custom_messages.rs src/result_in_tests.rs src/test_subset.rs
 
 src/lib.rs:
 src/assert_for_result.rs:
 src/testing_equality.rs:
 src/custom_messages.rs:
 src/result_in_tests.rs:
+src/test_subset.rs:
